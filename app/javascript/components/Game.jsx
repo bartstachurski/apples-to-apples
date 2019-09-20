27d3fed060ccca67b66de9ae24e91e9009fbae33
YOUR NAME <adien330@yahoo.com>
2019-09-19 22:00:57 -0400

    bootstrap add

--- app/javascript/components/Game.jsx
+++ app/javascript/components/Game.jsx
@@ -1,8 +1,23 @@
 import React from 'react';
+import Button from 'react-bootstrap/Button'
 
 
 const Game = props => (
-  <div>Hello from the game component!</div>
+    <div>Hello from the game component!</div> <
+    ButtonToolbar >
+    <Button variant="primary">Primary</Button> <
+    Button variant = "secondary" > Secondary < /Button> <
+    Button variant = "success" > Success < /Button> <
+    Button variant = "warning" > Warning < /Button> <
+    Button variant = "danger" > Danger < /Button> <
+    Button variant = "info" > Info < /Button> <
+    Button variant = "light" > Light < /Button> <
+    Button variant = "dark" > Dark < /Button> <
+    Button variant = "link" > Link < /Button> < /
+    ButtonToolbar >
 );
+
+
+
 
 export default Game;
