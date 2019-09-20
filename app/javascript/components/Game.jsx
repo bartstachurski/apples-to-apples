36814415eebb3d28ab88c59ed5599ee58088a903
bartstachurski <bart.stachurski@gmail.com>
2019-09-20 08:19:18 -0400

    add new game button component and show on home page. no functionality yet though

--- app/javascript/components/Game.jsx
+++ app/javascript/components/Game.jsx
@@ -1,23 +1,8 @@
 import React from 'react';
-import Button from 'react-bootstrap/Button'
-
 
 const Game = props => (
-    <div>Hello from the game component!</div> <
-    ButtonToolbar >
-    <Button variant="primary">Primary</Button> <
-    Button variant = "secondary" > Secondary < /Button> <
-    Button variant = "success" > Success < /Button> <
-    Button variant = "warning" > Warning < /Button> <
-    Button variant = "danger" > Danger < /Button> <
-    Button variant = "info" > Info < /Button> <
-    Button variant = "light" > Light < /Button> <
-    Button variant = "dark" > Dark < /Button> <
-    Button variant = "link" > Link < /Button> < /
-    ButtonToolbar >
+  <div>Hello from the game component!</div> 
 );
-
-
 
 
 export default Game;
---
+++ app/javascript/components/NewGameButton.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Button from 'react-bootstrap/Button';
+
+const NewGameButton = props => (
+  <div>
+    <Button variant="primary" size="lg" block>
+      Start New Game
+    </Button>
+  </div>
+);
+
+export default NewGameButton;
